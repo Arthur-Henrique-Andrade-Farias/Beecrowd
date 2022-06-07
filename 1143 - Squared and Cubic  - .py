947fd1af0d5fafb5227b33be47d3c0b63ea570f5
Arthur Henrique Andrade Farias <101343526+arthurHAF@users.noreply.github.com>
2022-06-07 15:02:48 -0300

    Repetition + multip.
    Python issue.

---
+++ 1143 - Squared and Cubic  - .py
@@ -0,0 +1,13 @@
+n = int(input())
+print("1 1 1")
+n1 = 1
+n2 = 2
+n3 = 3
+m = 2 
+
+for t in range(n):
+    n1 = n1 * m
+    n2 = n2 * m
+    n3 = n3 * m
+    print("{} {} {}".format())
+    m += 1 
